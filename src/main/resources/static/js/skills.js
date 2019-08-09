54faf94d9c97a28a9eaa8bac9dcec693d65b844b
Anton Shelenkov <anshelen@yandex.ru>
2019-08-09 13:42:49 +0300

    feat: Redesign site

---
+++ src/main/resources/static/js/skills.js
@@ -0,0 +1,12 @@
+'use strict';
+
+
+$().ready(function () {
+  $('[data-toggle="popover"]').popover({
+    html: true,
+    content: function () {
+      let id = $(this).data('details');
+      return $('#' + id).html();
+    }
+  });
+});
