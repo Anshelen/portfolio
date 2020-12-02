ed964c05f4554baf2455b16708f76ac35c8bba52
Anton Shelenkov <anshelen@yandex.ru>
2020-12-02 16:55:07 +0300

    refactor: Move all error handling logic to one controller advice

--- src/main/java/dev/shelenkov/portfolio/web/error/ErrorHandlingControllerAdvice.java
+++ src/main/java/dev/shelenkov/portfolio/web/error/ErrorHandlingControllerAdvice.java
@@ -2,6 +2,7 @@ package dev.shelenkov.portfolio.web.error;
 
 import dev.shelenkov.portfolio.web.wrappers.error.ValidationErrorResponse;
 import dev.shelenkov.portfolio.web.wrappers.error.Violation;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -9,12 +10,32 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+import org.springframework.web.server.ResponseStatusException;
 
 import javax.validation.ConstraintViolation;
 import javax.validation.ConstraintViolationException;
 
 @ControllerAdvice
-public class ValidationErrorHandlingControllerAdvice {
+@Slf4j
+public class ErrorHandlingControllerAdvice {
+
+    @ExceptionHandler(CorruptedIpException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public void onCorruptedIpException(CorruptedIpException e) {
+        log.warn(e.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public void onMethodArgumentTypeMismatchException(MethodArgumentTypeMismatchException e) {
+        log.warn(e.getMessage());
+    }
+
+    @ExceptionHandler(ResponseStatusException.class)
+    public void onResponseStatusException(ResponseStatusException e) {
+        throw e;
+    }
 
     @ExceptionHandler(ConstraintViolationException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
@@ -40,5 +61,11 @@ public class ValidationErrorHandlingControllerAdvice {
                 new Violation(fieldError.getField(), fieldError.getDefaultMessage()));
         }
         return error;
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public void onUnexpectedRuntimeException(RuntimeException e) {
+        log.error("Unexpected exception", e);
     }
 }
