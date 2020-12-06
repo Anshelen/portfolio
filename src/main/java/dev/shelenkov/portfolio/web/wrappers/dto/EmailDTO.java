daef29f73f77dc75eebd3c3470badf340cfdd28d
Anton Shelenkov <anshelen@yandex.ru>
2020-12-06 20:41:04 +0300

    refactor: Refactor DTO classes

--- src/main/java/dev/shelenkov/portfolio/web/wrappers/dto/EmailDTO.java
+++ src/main/java/dev/shelenkov/portfolio/web/wrappers/dto/EmailDTO.java
@@ -1,22 +1,26 @@
 package dev.shelenkov.portfolio.web.wrappers.dto;
 
-import lombok.Value;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
-@Value
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class EmailDTO {
 
     @NotNull
     @Size(min = 2, max = 30)
-    private final String name;
+    private String name;
 
     @NotNull
     @Size(min = 2, max = 50)
-    private final String subject;
+    private String subject;
 
     @NotNull
     @Size(max = 999)
-    private final String text;
+    private String text;
 }
