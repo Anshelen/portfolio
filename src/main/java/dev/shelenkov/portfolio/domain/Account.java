fe59b9ac0ebbcfa428d2c50cb2e167cd013f9fe1
Anton Shelenkov <anshelen@yandex.ru>
2020-12-24 22:24:29 +0300

    Add country domain object

--- src/main/java/dev/shelenkov/portfolio/domain/Account.java
+++ src/main/java/dev/shelenkov/portfolio/domain/Account.java
@@ -1,26 +1,31 @@
 package dev.shelenkov.portfolio.domain;
 
 import lombok.AccessLevel;
-import lombok.Getter;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 import org.hibernate.annotations.Type;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Version;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.EnumSet;
+import java.util.HashSet;
 import java.util.Set;
 
 @Entity
-@Getter
-@Setter
+@Data
 @NoArgsConstructor
 public class Account {
 
@@ -37,6 +42,7 @@ public class Account {
     @Column(unique = true)
     private String email;
 
+    @ToString.Exclude
     private String password;
 
     @Version
@@ -54,6 +60,19 @@ public class Account {
     @Type(type = "dev.shelenkov.portfolio.repository.support.SetOfRolesType")
     @Column(columnDefinition = "user_role[]")
     private Set<Role> roles = EnumSet.noneOf(Role.class);
+
+    /**
+     * Countries from which user tried to log in.
+     */
+    @ToString.Exclude
+    @Setter(AccessLevel.NONE)
+    @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    @JoinTable(
+        name = "account_x_country",
+        joinColumns = @JoinColumn(name = "account_id"),
+        inverseJoinColumns = @JoinColumn(name = "country_id")
+    )
+    private Set<Country> loginCountries = new HashSet<>();
 
     public Account(String username, String email, String password,
                    Collection<Role> roles) {
@@ -81,5 +100,21 @@ public class Account {
 
     public Set<Role> getRoles() {
         return Collections.unmodifiableSet(roles);
+    }
+
+    public void addLoginCountry(Country country) {
+        loginCountries.add(country);
+    }
+
+    public void removeLoginCountry(Country country) {
+        loginCountries.remove(country);
+    }
+
+    public boolean containsLoginCountry(Country country) {
+        return loginCountries.contains(country);
+    }
+
+    public Set<Country> getLoginCountries() {
+        return Collections.unmodifiableSet(loginCountries);
     }
 }
